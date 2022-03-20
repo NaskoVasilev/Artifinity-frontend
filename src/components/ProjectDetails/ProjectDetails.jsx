87595369a637cb6f07a90f6620bfc7b01ace6155
Rosen Santev <r.ssantev@gmail.com>
2022-03-20 15:51:02 +0200

    invest modal

--- src/components/ProjectDetails/ProjectDetails.jsx
+++ src/components/ProjectDetails/ProjectDetails.jsx
@@ -11,117 +11,120 @@ import classes from './ProjectDetails.module.scss';
 
 
 const ProjectDetails = (props) => {
-    const [project, setProject] = useState();
-    const [percentage, setPercentage] = useState(0);
-    const [modalShow, setModalShow] = useState(false);
+  const [project, setProject] = useState();
+  const [percentage, setPercentage] = useState(0);
+  const [modalShow, setModalShow] = useState(false);
 
-    const { account } = useStoreState((state) => state.walletStore);
+  const { account } = useStoreState((state) => state.walletStore);
 
-    const location = useLocation()
-    const navigate = useNavigate()
+  const location = useLocation()
+  const navigate = useNavigate()
 
-    useEffect(() => {
-        loadData()
-    }, []);
+  useEffect(() => {
+    loadData()
+  }, []);
 
-    const loadData = async () => {
-        if (!location.state.id) {
-            navigate('/')
-        }
-
-        const data = await ProjectService.details(location.state.id)
-
-        const now = +(Date.now() / 1000).toFixed()
-        const start = (new Date(data.createdOn).getTime() / 1000).toFixed()
-        const end = (new Date(data.endDate).getTime() / 1000).toFixed()
-        setPercentage((100 * (now - start) / (end - start)).toFixed())
-
-        const info = await BlockchainService.getProjectData(data.contractId)
-        data.totalInvestment = ethers.utils.formatEther(ethers.BigNumber.from(info.totalInvestment))
-        setProject(data)
+  const loadData = async () => {
+    if (!location.state.id) {
+      navigate('/')
     }
 
-    const investHandler = async (e) => {
-        e.preventDefault()
-        setModalShow(false)
+    const data = await ProjectService.details(location.state.id)
 
-        const amount = +e.target.elements.amount.value
-        await BlockchainService.invest(project.contractId, amount, account.address)
-        setProject({ ...project, totalInvestment: project.totalInvestment + amount })
-    }
+    const now = +(Date.now() / 1000).toFixed()
+    const start = (new Date(data.createdOn).getTime() / 1000).toFixed()
+    const end = (new Date(data.endDate).getTime() / 1000).toFixed()
+    setPercentage((100 * (now - start) / (end - start)).toFixed())
 
-    if (!project) {
-        return <Spinner />
-    }
+    const info = await BlockchainService.getProjectData(data.contractId)
+    data.totalInvestment = ethers.utils.formatEther(ethers.BigNumber.from(info.totalInvestment))
+    setProject(data)
+  }
 
-    // TODO fix styling, arrange elements
-    return (<>
-        <div className={classes.TitlesContainer}>
-            <h1 className={classes.Heading}>{project.name}</h1>
-            <p className={classes.Description}>{project.description}</p>
-            <div className={classes.Badges}>
-                {project.tags.map((tag, i) => {
-                    return <Badge className={classes.Badge} pill bg="warning" key={i}>
-                        {tag}
-                    </Badge>
-                })}
-            </div>
-        </div>
+  const investHandler = async (e) => {
+    e.preventDefault()
+    setModalShow(false)
 
-        <Row>
-            <Col md={8}>
-                <Image thumbnail alt='project idea' src={project.imageUrl} />
-            </Col>
-            <Col>
-                <p className={classes.Period}>{new Date(project.createdOn).toLocaleString()} - {new Date(project.endDate).toLocaleString()}</p>
-                <p className='m-0 fw-bold'>
-                    Completed: {percentage}%
-                </p>
-                <ProgressBar className={classes.ProgressContainer} variant='warning' now={percentage} />
-                <p className='mt-2'>
-                    <a className={classes.Link}href={project.url}>Link to project</a>
-                </p>
-                <p>Total investment: {project.totalInvestment} MTK</p>
-                {/* TODO add value & price */}
-                {/* TODO add invest button */}
-                <Button className="fw-bold" variant="warning" onClick={() => setModalShow(true)}>
-                    Invest
-                </Button>
-            </Col>
-        </Row>
-        <div className={classes.ArtistContainer}>
-            <h3>About the author</h3>
-            <p>{project.owner.firstName} {project.owner.lastName}</p>
-            <p>{project.owner.bio}</p>
-            <p>
-                <a className={classes.Link}href={project.owner.portfolioUrl}>Read more</a>
-            </p>
-            
-        </div>
+    const amount = +e.target.elements.amount.value
+    await BlockchainService.invest(project.contractId, amount, account.address)
+    setProject({ ...project, totalInvestment: project.totalInvestment + amount })
+  }
 
-        <Modal
-            show={modalShow}
-            size="sm"
-            onHide={() => setModalShow(false)}
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-        >
-            <Modal.Header closeButton>
-                <Modal.Title id="contained-modal-title-vcenter">
-                    Investment size
-                </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <Form onSubmit={investHandler}>
-                    <Form.Group className="mb-3" controlId="amount">
-                        <Form.Label>Value</Form.Label>
-                        <Form.Control type="number" placeholder="Value" />
-                    </Form.Group>
-                    <Button type='submit'>Invest</Button>
-                </Form>
-            </Modal.Body>
-        </Modal>
-    </>)
+  if (!project) {
+    return <Spinner />
+  }
+
+  // TODO fix styling, arrange elements
+  return (<>
+    <div className={classes.TitlesContainer}>
+      <h1 className={classes.Heading}>{project.name}</h1>
+      <p className={classes.Description}>{project.description}</p>
+      <div className={classes.Badges}>
+        {project.tags.map((tag, i) => {
+          return <Badge className={classes.Badge} pill bg="warning" key={i}>
+            {tag}
+          </Badge>
+        })}
+      </div>
+    </div>
+
+    <Row>
+      <Col md={8}>
+        <Image thumbnail alt='project idea' src={project.imageUrl} />
+      </Col>
+      <Col>
+        <p className={classes.Period}>{new Date(project.createdOn).toLocaleString()} - {new Date(project.endDate).toLocaleString()}</p>
+        <p className='m-0 fw-bold'>
+          Completed: {percentage}%
+        </p>
+        <ProgressBar className={classes.ProgressContainer} variant='warning' now={percentage} />
+        <p className='mt-2'>
+          <a className={classes.Link} href={project.url}>Link to project</a>
+        </p>
+        <p>Total investment: {project.totalInvestment} MTK</p>
+        {/* TODO add value & price */}
+        {/* TODO add invest button */}
+        <Button className="fw-bold" variant="warning" onClick={() => setModalShow(true)}>
+          Invest
+        </Button>
+      </Col>
+    </Row>
+    <div className={classes.ArtistContainer}>
+      <h3>About the author</h3>
+      <p>{project.owner.firstName} {project.owner.lastName}</p>
+      <p>{project.owner.bio}</p>
+      <p>
+        <a className={classes.Link} href={project.owner.portfolioUrl}>Read more</a>
+      </p>
+
+    </div>
+
+    <Modal
+      show={modalShow}
+      size="sm"
+      onHide={() => setModalShow(false)}
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+
+      <div className={classes.InvestModal} >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Investment size
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={investHandler}>
+            <Form.Group className="mb-3" controlId="amount">
+              <Form.Label>Value</Form.Label>
+              <Form.Control type="number" placeholder="Value" />
+            </Form.Group>
+            <Button className="fw-bold" variant="warning" type='submit'>Invest</Button>
+          </Form>
+        </Modal.Body>
+      </div>
+    </Modal>
+  </>)
 }
 
 export default ProjectDetails;
